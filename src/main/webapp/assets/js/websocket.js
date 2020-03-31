3364c0cfee6101fc2f39479320744ef26fd92867
bogdan-todorovic <bogdantodorovic93@gmai.com>
2020-03-31 16:27:23 +0200

    basic client api for testing

--- src/main/webapp/assets/js/websocket.js
+++ src/main/webapp/assets/js/websocket.js
@@ -1,32 +1,33 @@
-var socket;
+var obj = {};
 
-function initializeConnection() {
-    
-    const endpoint = "/echo";
+obj.initializeConnection = function() {
+
+    const endpoint = "/test";
     if (window.location.protocol === "http:") {
-        socket = new WebSocket("ws://" + window.location.host + endpoint);
+        obj.socket = new WebSocket("ws://" + window.location.host + endpoint);
     } else {
-        socket = new WebSocket("wss://" + window.location.host + endpoint);
+        obj.socket = new WebSocket("wss://" + window.location.host + endpoint);
     }
 
-    socket.onopen = function(event) {
+    obj.socket.onopen = function(event) {
         console.log("WebSocket is open now.");
-        socket.send("Message to be sent");
     };
 
-    socket.onclose = function(event) {
+    obj.socket.onclose = function(event) {
         console.log("WebSocket is closed now.");
     };
 
-    socket.onmessage = function(event) {
+    obj.socket.onmessage = function(event) {
+        console.log("Sending message.");
         console.log("WebSocket message received: ", event);
     };
 
-    socket.onerror = function(event) {
-
+    obj.socket.onerror = function(event) {
     };
 }
+obj.initializeConnection();
 
 document.querySelector("#btn").addEventListener("click", function() {
-    initializeConnection();
+    var textInput = document.querySelector("#inputBox").value;
+    obj.socket.send(textInput);
 })
