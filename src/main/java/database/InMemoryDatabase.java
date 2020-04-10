927f62eb3580fe51a36e358f163b18ce7ec6d8dd
bogdan-todorovic <bogdantodorovic93@gmai.com>
2020-04-10 15:31:44 +0200

    temporary class for storing connected sockets

---
+++ src/main/java/database/InMemoryDatabase.java
@@ -0,0 +1,11 @@
+package database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.Session;
+
+public class InMemoryDatabase {
+    public static List<Session> players = new ArrayList<>();
+    
+}
