ad7fd13c816e11205ebcaf450405127e0f426199
bogdan-todorovic <bogdantodorovic93@gmail.com>
2021-07-20 13:16:02 +0200

    refactor word higlighting

--- src/main/client/src/pages/GamePage.js
+++ src/main/client/src/pages/GamePage.js
@@ -1,15 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { useDispatch, } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { sendWord } from "../reducers/gameReducer";
 import PropTypes from "prop-types";
 
 const WordWrap = ({ word }) => {
   const highlight = {
-    backgroundColor: "green"
+    color: "#99cc99"
   };
-  return(
-    <span style={word.isTyped ? highlight : {} }> {word.value} </span>
+  const underline = {
+    textDecoration: "underline"
+  };
+
+  return (
+    <span style=
+      {
+        Object.assign(
+          word.isTyped ? highlight : {},
+          word.isActive ? underline : {}
+        )
+      }
+    >{word.value}</span>
   );
 };
 WordWrap.propTypes = {
@@ -18,42 +29,60 @@ WordWrap.propTypes = {
 
 const GamePage = () => {
   // converting string quote to word objects to track the changes
-  // const quote = useSelector(state => state.game.quote);
-  // const quoteArray = quote.split(" ");
-  const quoteArrayTest = ["this", "is", "just", "a", "test", "string"];
-  const wordObjects = quoteArrayTest.map(elem => ({ value: elem, isTyped: false }));
+  const quoteString = useSelector(state => state.game.quote);
+  const wordsArray = quoteString.split(" ");
+  // const quoteArrayTest = ["this", "is", "just", "a", "test", "string"];
+  const wordObjects =
+    wordsArray
+      .map((elem, idx) => {
+        return {
+          value: elem,
+          isTyped: false,
+          isActive: idx === 0 ? true : false
+        };
+      });
   const [words, setWords] = useState(wordObjects);
-  const [word, setWord] = useState("");
-  const [currentWord, setCurrentWord] = useState(0);
+  const [typedWord, setTypedWord] = useState("");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const dispatch = useDispatch();
 
   const handleWord = e => {
     if (e.code === "Space") {
       checkWord();
-      dispatch(sendWord(word));
-      setWord("");
+      dispatch(sendWord(typedWord));
+      setTypedWord("");
       e.target.value = "";
       e.preventDefault();
     }
   };
   const checkWord = () => {
-    if (word === words[currentWord].value) {
+    if (typedWord === words[currentWordIndex].value) {
       const wordsCopy = [...words];
-      wordsCopy[currentWord].isTyped = true;
-      setCurrentWord(currentWord + 1);
+      wordsCopy[currentWordIndex].isTyped = true;
+      wordsCopy[currentWordIndex].isActive = false;
+      if (words.length <= currentWordIndex) {
+        wordsCopy[currentWordIndex+1].isActive = true;
+      }
+      setCurrentWordIndex(currentWordIndex + 1);
       setWords(wordsCopy);
     }
   };
 
   return (
-    <div>
+    <div className="container game-container">
       <p>
-        {words
-          .map((elem, idx) =>
-            <WordWrap key={idx} word={elem} />)
+        {
+          words
+            .map((elem, idx) =>
+              <WordWrap key={idx} word={elem} />
+            )
         }
       </p>
-      <input type="text" onKeyDown={handleWord} onChange={e => setWord(e.target.value)} />
+      <input
+        type="text"
+        onKeyDown={handleWord}
+        onChange={e => setTypedWord(e.target.value)}
+      />
     </div>
   );
 };
